a19ced6513b94b8f4e6247206e44798f55ba63a4
unknown <mattheusjuca@gmail.com>
2017-07-25 11:11:37 -0300

    Create a node server

---
+++ index.js
@@ -0,0 +1,14 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function (){
+  console.log('listening on *:3000');
+});
---
+++ public/scripts/main.js
@@ -0,0 +1,72 @@
+Vue.component('category-item', {
+  props: ['category'],
+  template: `
+  <li>
+    <span class="category-title">
+      {{ category.text }}
+    </span>
+    <ul>
+      <li v-for="item in category.itens">
+        <div class="item-simbol">
+          <img :src="item.img"/>
+        </div>
+        <div>
+          <strong>
+            {{ item.name }}
+          </strong>
+          <br />
+          {{item.description}}
+        </div>
+      </li>
+    </ul>
+  </li>`
+})
+
+var app7 = new Vue({
+  el: '#app',
+  data: {
+    categories: [
+      { id: 0, 
+        text: 'LANCHE', 
+        itens: [
+          {
+            name: 'Sanduíche de Peito de Peru',
+            description: 'Sanduíche de peito de peru servido com queijo prato no pão com gergelim',
+            img: '/images/sanduiche.png'
+          }
+        ] 
+      },
+      { id: 1, 
+        text: 'BEBIDA', 
+        itens: [
+          {
+            name: 'Coca-Cola', 
+            description: 'Latinha de 175mL',
+            img: '/images/cocacola.png'
+          }, 
+          {
+            name: 'Guaraná',
+            img: '/images/guarana.png'
+          }, 
+          {
+            name: 'Suco de Laranja', 
+            img: '/images/maguary.png'
+          }
+        ]
+      },
+      { id: 2, 
+        text: 'OUTROS', 
+        itens: [
+          {
+            name: 'Fone de Ouvido', 
+            img: '/images/fone.png'
+          }, 
+          {
+            name: 'Cobertor', 
+            img: '/images/cobertor.png'
+          }
+        ] 
+      }
+    ]
+  }
+})
