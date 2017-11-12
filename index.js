704745b9e68f5510e35ab221c0b972d9d13e4fe8
unknown <mattheusjuca@gmail.com>
2017-11-12 18:29:00 -0200

    Update dextrap

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function (){
-  console.log('listening on *:3000');
+http.listen(8080, function (){
+  console.log('listening on *:8080');
 });
